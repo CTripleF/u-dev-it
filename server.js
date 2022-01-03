e4b5548cc38573b980a827e550dd6b92a530ecac
CTripleF <cjfabra1@gmail.com>
2022-01-03 17:27:32 -0500

    connected database to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // imports
 const express = require('express');
+const mysql = require('mysql2');
+
 
 //port designation
 const PORT = process.env.PORT ||3002;
@@ -8,6 +10,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({extend: false}));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '26374891ZAxs!@',
+    database: 'election'
+  },
+  console.log('connected to database election')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+})
 
 app.get('/',(req, res) => {
   res.json({
